178b7e3edf32e11e313fff2aaaafac2193acdd5a
Jacob van Steyn <jacobvs2@gmail.com>
2020-04-05 15:54:23 -0700

    Fix require_once errors

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ Scrapes character information for a specific player
 */
 declare(strict_types=1);
 
-require_once('classes/RealmEyeAPIUtils.php');
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'classes/RealmEyeAPIUtils.php');
 new RealmEyeAPIUtils();
 $config = RealmEyeAPIUtils::$config;
 $logger = RealmEyeAPIUtils::$logger;
@@ -64,7 +65,7 @@ if (
 	echo_json_and_exit(['error' => 'Invalid callback name']);
 }
 
-require_once 'items.php'; // import items definitions
+require_once(__ROOT__.'items.php'); // import items definitions
 
 // set up some initial vars
 $final_output = [];
